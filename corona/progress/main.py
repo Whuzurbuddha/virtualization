2827250c009c757728a5b1cb000b4d65d0583091
Alexander Päplow <116446513+Whuzurbuddha@users.noreply.github.com>
2024-07-07 21:49:44 +0200

    Update main.py

--- corona/progress/main.py
+++ corona/progress/main.py
@@ -3,19 +3,20 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from matplotlib.ticker import MaxNLocator
 
+import sys
+sys.path.append('/home/alexander/devel/python')
+from database_access import data_handler
+
 
 class Database:
-    host = "127.0.0.1"
-    port = "3306"
-    user = username
-    password = pwd
-
     def __init__(self):
+        filepath = data_handler.Data.path
+        data = data_handler.read_json(filepath)
         self.connection = mysql.connector.connect(
-            host=self.host,
-            port=self.port,
-            user=self.user,
-            password=self.password,
+            host=data["host"],
+            port=data["port"],
+            user=data["user"],
+            password=data["password"],
             database="corona"
         )
 
@@ -43,20 +44,24 @@ def add_cases_plot(data):
 
     fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, sharex=True, figsize=(10, 8))
 
+    # Plotting the bar chart for cases on the first subplot
     ax1.bar(date, cases, width=0.5, color='skyblue', edgecolor='darkblue', linewidth=0.7, label='Cases')
     ax1.set_ylabel('Cases per 100k')
     ax1.set_title('COVID-19 Cases and Deaths Over Time')
     ax1.legend()
 
+    # Plotting the line chart for deaths on the second subplot
     ax2.plot(date, deaths, color='red', linewidth=2, label='Deaths')
     ax2.set_xlabel('Date')
     ax2.set_ylabel('Deaths')
     ax2.legend()
 
+    # Formatting the date axis
     ax2.xaxis.set_major_locator(mdates.MonthLocator())
     ax2.xaxis.set_major_formatter(mdates.DateFormatter('%m.%Y'))
     plt.xticks(rotation=45)
 
+    # Setting y-axis ticks to avoid overlap
     ax2.yaxis.set_major_locator(MaxNLocator(prune='lower', nbins=10))
 
     plt.tight_layout()
